6875d7234448f1320bfa50c1cadfb2720d32e9ee
Ali Karami <kaali728@yahoo.com>
2021-10-31 16:25:32 +0100

    add export

--- src/components/Export.js
+++ src/components/Export.js
@@ -1,8 +1,26 @@
 import React from "react";
+import { useRecoilValue } from "recoil";
+
+import { ImageUrl as imageUrl } from "../services/atoms";
+import { CourseStructure as courseStructure } from "../services/atoms";
+import { LearningObjectives as learningObjectives } from "../services/atoms";
+import { CourseNavigation as courseNavigation } from "../services/atoms";
+import { Contact as contact } from "../services/atoms";
 
 export default function Export() {
+  const imageUrlObjects = useRecoilValue(imageUrl);
+  const courseStructureObjects = useRecoilValue(courseStructure);
+  const learningObjectivesObjects = useRecoilValue(learningObjectives);
+  const courseNavigationObjects = useRecoilValue(courseNavigation);
+  const contacts = useRecoilValue(contact);
+
   const exportData = () => {
     console.log("Exporting...");
+    console.log(imageUrlObjects);
+    console.log(courseStructureObjects);
+    console.log(learningObjectivesObjects);
+    console.log(courseNavigationObjects);
+    console.log(contacts);
   };
   return (
     <div>
--- src/components/view/Contacts.js
+++ src/components/view/Contacts.js
@@ -1,10 +1,35 @@
 import React from "react";
+import { useRecoilValue } from "recoil";
+import { Contact as contact } from "../../services/atoms";
 
 function Contacts() {
+  const contacts = useRecoilValue(contact);
+
+  function contactItem({ profilePicture, name, email }) {
+    return (
+      <div className="vContactWrapper">
+        <div className="vContactleft">
+          <img src={profilePicture} className="vHeaderImage" />
+        </div>
+        <div className="vContactRight">
+          <span className="vContactName">{name}</span>
+          <a className="vContactEmail" href={`mailto:${email}`}>
+            {email}
+          </a>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="vContactsWrapper">
       <div className="vCoursenavigationHeader">Contacts</div>
       <div className="vViereck"></div>
+      <div className="vContactItemsWrapper">
+        {contacts.contacts.map((contact, index) => {
+          return contactItem(contact);
+        })}
+      </div>
     </div>
   );
 }
