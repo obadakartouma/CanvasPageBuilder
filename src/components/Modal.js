abb0c310f8089507e71582739f97e27a0a183114
Ali Karami <kaali728@yahoo.com>
2021-11-25 18:22:42 +0100

    latest commit

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 export default function Modal({ handleClose, show, children }) {
   const showHideClassName = show ? "modal display-block" : "modal display-none";
+  const [copySuccess, setCopySuccess] = useState("");
+  const textAreaRef = useRef(null);
+
+  function copyToClipboard(e) {
+    textAreaRef.current.select();
+    document.execCommand("copy");
+    // This is just personal preference.
+    // I prefer to not show the the whole text area selected.
+    e.target.focus();
+    setCopySuccess("Copied!");
+  }
+
   return (
     <div className={showHideClassName}>
       <section className="modal-main">
-        <input className="modalInput" value={children} />
-        <button type="button" onClick={handleClose}>
+        <div className="modal-header">
+          <h2>Copy and Paste in your Canvas Page as Html</h2>
+          {document.queryCommandSupported("copy") && (
+            <>
+              <button
+                className="h_button"
+                style={{ marginBottom: "10px" }}
+                onClick={copyToClipboard}
+              >
+                Copy
+              </button>
+              {copySuccess}
+            </>
+          )}
+        </div>
+        <label className="textareaContainer">
+          <textarea
+            ref={textAreaRef}
+            className="modal-input"
+            value={children}
+          />
+        </label>
+
+        <button className="modal-button" onClick={handleClose}>
           Close
         </button>
       </section>
